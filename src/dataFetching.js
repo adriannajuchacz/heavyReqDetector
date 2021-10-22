b726e6b1db28b2369cca8522daddf55c1f2a8160
Adrianna Juchacz <adrianna.juchacz@aaron.ai>
2021-10-22 11:27:12 +0200

    added generation of statistics

--- src/dataFetching.js
+++ src/dataFetching.js
@@ -1,19 +1,105 @@
 var awsCli = require('aws-cli-js');
+const jsonfile = require('jsonfile');
+var fs = require('fs');
+
 var Options = awsCli.Options;
 var Aws = awsCli.Aws;
 
 var options = new Options(
-    process.env.ACCESS_KEY,         /* accessKey    */ 
-    process.env.SECRET_KEY,         /* secretKey    */ 
-    null,                           /* sessionToken */ 
-    null,                           /* currentWorkingDirectory */ 
-    'aws'                           /* cliPath */ 
+    process.env.ACCESS_KEY,         /* accessKey    */
+    process.env.SECRET_KEY,         /* secretKey    */
+    null,                           /* sessionToken */
+    null,                           /* currentWorkingDirectory */
+    'aws'                           /* cliPath */
 );
 
 var aws = new Aws(options);
+var config = JSON.parse(fs.readFileSync('config/config_file.json', 'utf8'));
+const start_time_UTC = new Date(config.start_time).getTime()
+const end_time_UTC = new Date(config.end_time).getTime()
+const statsToFetch = [
+    {
+        "statsType": "avg(responseTime)",
+        "label": "avg_responseTime"
+    },
+    {
+        "statsType": "pct(responseTime, 95)",
+        "label": "pct95_responseTime"
+    },
+    {
+        "statsType": "pct(responseTime, 99)",
+        "label": "pct99_responseTime"
+    },
+    {
+        "statsType": "pct(responseTime, 99.5)",
+        "label": "pct99_5_responseTime"
+    }
+]
 
-aws.command('ec2 describe-images').then(function (data) {
-    console.log('data = ', data);
-}).catch((e) => {
-    console.log(e)
-});
+async function runQuery(statsType, regex) {
+    // start the query 
+    let query_id
+    await aws.command(`logs start-query --log-group-name prod.aaron.ai --start-time ${start_time_UTC} --end-time ${end_time_UTC} --query-string 'fields @timestamp, @message, responseTime | filter @logStream like "biz" and message like "request completed" and req.url like ${regex} | stats ${statsType}'`).then(function (data) {
+        query_id = data.object.queryId
+    }).catch((e) => {
+        console.log(e)
+    });
+
+    // run the query
+    let status, res
+    while (status !== "Complete") {
+        res = await aws.command(`logs get-query-results --query-id ${query_id}`).then(function (data) {
+            status = data.object.status
+            if (status === 'Complete') {
+                console.log(parseFloat(data.object.results[0][0].value))
+                return parseFloat(data.object.results[0][0].value)
+            }
+        }).catch((e) => {
+            console.log(e)
+        });
+    }
+    return res;
+}
+/**
+ * Function takes an array
+ * 
+ * e.g.
+ *  {
+        "url": "/v/dev-0.0/tenants/bmg/subtenants/XXXXX/calls",
+        "count": 213,
+        "regex": /\/v\/dev-0.0\/tenants\/bmg\/subtenants\/(.*)\/calls/
+    }
+ * =>
+    {
+        "url": "/v/dev-0.0/tenants/bmg/subtenants/XXXXX/calls",
+        "count": 213,
+        "regex": /\/v\/dev-0.0\/tenants\/bmg\/subtenants\/(.*)\/calls/, 
+        "avgResponseTime": ,
+        "pct95": ,
+        "pct99": ,
+        "pct99_5":
+    }
+ * 
+ * @param {array} urlArray 
+ */
+async function fetchResponseTimeData(urlArray) {
+    for (let i = 0; i < urlArray.length; i++) {
+        console.log(`processing: ${i}/${urlArray.length-1}`)
+        for (let j = 0; j < statsToFetch.length; j++) {
+            console.log(`       substep: ${statsToFetch[j].statsType}`)
+            let statResult = await runQuery(statsToFetch[j].statsType, urlArray[i].regex)
+            console.log(statResult)
+            urlArray[i][statsToFetch[j].label] = statResult
+        }
+        console.log('============')
+    }
+
+    //save to json file
+    jsonfile.writeFile("./data/urlsWithData.json", urlArray, { spaces: 2 }, function (err) {
+        if (err) console.error(err)
+    })
+}
+
+module.exports = {
+    fetchResponseTimeData
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const { groupAndCountUrls } = require('./urlGrouping.js');
 const { generateRegex } = require('./regexGenerating.js');
+const { fetchResponseTimeData } = require('./dataFetching.js');
+
 
 const main = async () => {
-
     // STEP 1: fetch data and save to ../data/urls.csv
     // STEP 2: group & count URLs, save result to the ../data/groupedUrls.json
     let urlArray = await groupAndCountUrls();
     // STEP 3: translate URLs to Regex
-    urlArray = generateRegex(urlArray)
+    urlArray = await generateRegex(urlArray)
+    urlArray = await fetchResponseTimeData(urlArray)
 }
 
 main()
